48d46a4a20fe70d7c1ec1cbea94386f77b69a5b6
zerg959 <zerg959@yandex.ru>
2025-12-28 01:53:37 +0300

    cafes and filters

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -1,1 +1,12 @@
+import FilterCafes from './FilterCafes';
 
+const CafesTable = () => {
+  return (
+    <div className="cafesTable">
+      <FilterCafes />
+      <ul className="cardsList"></ul>
+    </div>
+  );
+};
+
+export default CafesTable;
--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,1 +1,11 @@
+const FilterCafes = () => {
+  return (
+    <div className="controls">
+      <select name="subway" id="subway">
+        <option value="All">Все</option>
+      </select>
+    </div>
+  );
+};
 
+export default FilterCafes;
